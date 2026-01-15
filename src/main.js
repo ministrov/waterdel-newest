4f0c28d507e4045f4ae46ced4a0d26f30f5eb474
Zhila <ministrov2018@gmail.com>
2026-01-15 16:27:54 +0300

    feat: end up creating a desktop styling for reviews section

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-console.log('cccc');
+jQuery(document).ready(function ($) {
+  const reviewsSwiper = new Swiper('.reviews-slider', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    initialSlide: 1,
+    loopAdditionalSlides: 1,
+    navigation: {
+      nextEl: '.navigation__button--next',
+      prevEl: '.navigation__button--prev',
+    },
+    pagination: {
+      el: '.reviews__pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      992: {
+        spaceBetween: 20,
+        slidesPerView: 2,
+      },
+      580: {
+        spaceBetween: 10,
+        slidesPerView: 2,
+      },
+    },
+  });
+});
